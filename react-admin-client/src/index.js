8b1112a6eb0ab4dc85e594c41a381c763b85b72c
lstabc <185251999@qq.com>
2020-05-17 21:10:25 +0800

    the first commit.

---
+++ react-admin-client/src/index.js
@@ -0,0 +1,14 @@
+/*
+入口js
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import 'antd/dist/antd.css'
+
+import App from './App'
+
+// 读取local中保存user, 保存到内存中
+
+
+// 将App组件标签渲染到index页面的div上
+ReactDOM.render(<App />, document.getElementById('root'))
