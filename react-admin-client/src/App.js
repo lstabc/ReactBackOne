bc990711e6c5e70c68d76f850292619029a886ff
lstabc <185251999@qq.com>
2020-05-17 21:18:03 +0800

    the commit20200517.

--- react-admin-client/src/App.js
+++ react-admin-client/src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import Login from './pages/login/login'
 import Admin from './pages/admin/admin'
@@ -12,12 +11,13 @@ export default class App extends Component {
 
   render () {
     return (
-      <BrowserRouter>
-        <Switch> {/*只匹配其中一个*/}
-          <Route path='/login' component={Login}></Route>
-          <Route path='/' component={Admin}></Route>
-        </Switch>
-      </BrowserRouter>
+      <div>asdfasdf</div>
+      // <BrowserRouter>
+      //   <Switch> {/*只匹配其中一个*/}
+      //     <Route path='/login' component={Login}></Route>
+      //     <Route path='/' component={Admin}></Route>
+      //   </Switch>
+      // </BrowserRouter>
     )
   }
 }
