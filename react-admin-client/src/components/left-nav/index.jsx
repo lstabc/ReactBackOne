0f3509078142d42d80f3d69561ea0556ed5c4a93
lstabc <185251999@qq.com>
2020-05-20 23:03:34 +0800

    commit at time 2020-05-20-2303

--- react-admin-client/src/components/left-nav/index.jsx
+++ react-admin-client/src/components/left-nav/index.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
 import './index.less'
-import { Menu, Button } from 'antd';
+import { Menu } from 'antd';
 import {
-  AppstoreOutlined,
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-  PieChartOutlined,
-  DesktopOutlined,
-  ContainerOutlined,
-  MailOutlined,
+    AppstoreOutlined,
+    PieChartOutlined,
+    DesktopOutlined,
+    ContainerOutlined,
+    MailOutlined,
 } from '@ant-design/icons';
 import logojpg from '../../assets/images/bglogo.jpg'
+import { Link } from 'react-router-dom'
 
 const { SubMenu } = Menu;
 
@@ -18,57 +17,52 @@ export default class LeftNav extends Component {
 
     state = {
         collapsed: false,
-      };
-    
-      toggleCollapsed = () => {
+    };
+
+    toggleCollapsed = () => {
         this.setState({
-          collapsed: !this.state.collapsed,
+            collapsed: !this.state.collapsed,
         });
-      };
+    };
 
     render() {
         return (
-            <div className='left-nav'>
-                <div className='logo'>
-                    <img src = {logojpg} alt='logojpg'></img>
-
-                </div>
-                <div>
-                    <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
-                        {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
-                    </Button>
-                    <Menu
-                        defaultSelectedKeys={['1']}
-                        defaultOpenKeys={['sub1']}
-                        mode="inline"
-                        theme="dark"
-                        inlineCollapsed={this.state.collapsed}
-                    >
-                        <Menu.Item key="1" icon={<PieChartOutlined />}>
-                            Option 1
-          </Menu.Item>
-                        <Menu.Item key="2" icon={<DesktopOutlined />}>
-                            Option 2
-          </Menu.Item>
-                        <Menu.Item key="3" icon={<ContainerOutlined />}>
-                            Option 3
-          </Menu.Item>
-                        <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
-                            <Menu.Item key="5">Option 5</Menu.Item>
-                            <Menu.Item key="6">Option 6</Menu.Item>
-                            <Menu.Item key="7">Option 7</Menu.Item>
-                            <Menu.Item key="8">Option 8</Menu.Item>
+            <div to='/' className='left-nav'>
+                <Link className='left-nav-header'>
+                    <img src={logojpg} alt='logojpg'></img>
+                    <h1>管理后台</h1>
+                </Link>
+                <Menu
+                    defaultSelectedKeys={['1']}
+                    defaultOpenKeys={['sub1']}
+                    mode="inline"
+                    theme="dark"
+                    inlineCollapsed={this.state.collapsed}
+                >
+                    <Menu.Item key="1" icon={<PieChartOutlined />}>
+                        首页
+                    </Menu.Item>
+                    <Menu.Item key="2" icon={<DesktopOutlined />}>
+                        数据分析
+                    </Menu.Item>
+                    <Menu.Item key="3" icon={<ContainerOutlined />}>
+                        数据清理
+                    </Menu.Item>
+                    <SubMenu key="sub1" icon={<MailOutlined />} title="数据来源">
+                        <Menu.Item key="5">公安数据</Menu.Item>
+                        <Menu.Item key="6">采集数据</Menu.Item>
+                        <Menu.Item key="7">监控数据</Menu.Item>
+                        <Menu.Item key="8">视频数据</Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="sub2" icon={<AppstoreOutlined />} title="应用平台">
+                        <Menu.Item key="9">网格系统</Menu.Item>
+                        <Menu.Item key="10">OA系统</Menu.Item>
+                        <SubMenu key="sub3" title="子系统">
+                            <Menu.Item key="11">图表展示</Menu.Item>
+                            <Menu.Item key="12">自动分析</Menu.Item>
                         </SubMenu>
-                        <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
-                            <Menu.Item key="9">Option 9</Menu.Item>
-                            <Menu.Item key="10">Option 10</Menu.Item>
-                            <SubMenu key="sub3" title="Submenu">
-                                <Menu.Item key="11">Option 11</Menu.Item>
-                                <Menu.Item key="12">Option 12</Menu.Item>
-                            </SubMenu>
-                        </SubMenu>
-                    </Menu>
-                </div>
+                    </SubMenu>
+                </Menu>
             </div>
         )
     }
